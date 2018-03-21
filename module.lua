edd3ead79a720fffdf1ff259ba449cb2c0edc378
Samuel Linde <samuel@dontblink.se>
2018-03-21 22:41:54 +0100

    Text

--- module.lua
+++ module.lua
@@ -18,7 +18,8 @@ end
 function M.content_update(name)
     print("sub module content update", name)
     if name == 'test.txt' then
-        text = resource.load_file(localized(name))
+        text = "go yada"
+        -- text = resource.load_file(localized(name))
     end
 end
 
