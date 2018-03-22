a8a935d12ec36ae901efff2c9593c32e775f34ea
Samuel Linde <samuel@dontblink.se>
2018-03-22 08:47:26 +0100

    Debugging

--- module.lua
+++ module.lua
@@ -3,12 +3,12 @@ local localized, CHILDS, CONTENTS = ...
 local M = {}
 
 local font = resource.load_font(localized "silkscreen.ttf")
-local text
+-- local text
 
 print "sub module init"
 
 function M.draw()
-    font:write(100, 100, text, 30, 1,1,1,1)
+    font:write(100, 100, "hey", 60, 1,1,1,1)
 end
 
 function M.unload()
@@ -17,9 +17,9 @@ end
 
 function M.content_update(name)
     print("sub module content update", name)
-    if name == 'text.txt' then
-        text = resource.load_file(localized(name))
-    end
+    -- if name == 'text.txt' then
+    --     text = resource.load_file(localized(name))
+    -- end
 end
 
 function M.content_remove(name)
